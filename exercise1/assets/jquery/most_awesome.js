de3e4ec245aa8778ffd96e54bed8cd2a63ce84b6
FlyingGibbon <mgruener@student.ethz.ch>
2017-03-24 16:11:29 +0100

    added text

--- exercise1/assets/jquery/most_awesome.js
+++ exercise1/assets/jquery/most_awesome.js
@@ -20,6 +20,7 @@ function changes(){
 	if(execute === 0){
 		return;
 	}
+
 	$("#butt1").css({
         	"background-color" : "transparent"
         	});
@@ -34,7 +35,22 @@ function changes(){
         	});
 	switch(statepics){
 		case 0:
-			$("#butt1").css({
+		var h1 = "Green Rolls"
+		var h2 = "Bruschetta"
+		var h3 = "Bruschette"
+		var h4 = "Eggplants"
+		var h5 = "Spicy Beans"
+		var h6 = "Meatballs"
+		
+		var t1 = "We serve a seasonal tasting menu that focuses on local ingredients. Our appetizers may vary during the year to always ensure the best quality.\n For the appetizers, we are famous for our bruschettas that we serve in several different variants."
+		var t2 = "Delicious green rolls from Taiwan. Avocado rolls with salmon on top. All wrapped with the finest Taiwanese algae."
+		var t3 = "Delicious italian appetizer 🇮🇹. Vegan Bruschette with beans and tomato topping"
+		var t4 = "The classic, reborn. Our chef has prepared these delicious bruschettes. With Pomegranate sprinkled on top of it. Yummy!"
+		var t5 = "Dish simplicity implies food multiplicity. With this slogan we offer a simple yet delicious appetizer, the filled Eggplants (The actual food in the image is filled peppers)" 
+		var t6 = "Fresh edamame beans straight out of Japan 🇯🇵. Go spicy or go home"		
+		
+
+		$("#butt1").css({
         	"background-color" : "rgba(0, 255, 0, 0.2)"
         	});
         	$("#firstimg").attr("src","images/pic02.jpg");
@@ -50,18 +66,44 @@ function changes(){
         	$("#sixthimg").attr("src","images/pic05.jpg");
         	$("#sixthimgpop").attr("src","images/pic05.jpg");
 
+		replace_text(h1,t1,$("#firstbox"), $("#openModal1"));	
+		replace_text(h2,t2,$("#secondbox"), $("#openModal2"));
+		replace_text(h3,t3,$("#thirdbox"), $("#openModal3"));
+		replace_text(h4,t4,$("#fourthbox"), $("#openModal4"));
+		replace_text(h5,t5,$("#fifthbox"), $("#openModal5"));
+		replace_text(h6,t5,$("#sixthbox"), $("#openModal6"));
+
 
 
 			break;
 
 		case 1:
+		
+		var h1= "Spaghetti Carbonara"
+            var h2 = "Penne Tuna"
+            var h3 = "Cannelloni"
+            var h4 = "Lasagne"
+            var h5 = "Cheese Macaroni"
+            var h6 = "Spaghetti Arrabitata"
+
+            var t1= "Homemade Spaghetti Carbonara. Creamy and delicious."
+            var t2 = "he combination of tuna, anchovies, capers, garlic, and parsley is traditional in Mediterranean cooking, particularly in southern France."
+            var t3 = "Cannelloni (Italian for large reeds) are a cylindrical type of pasta generally served baked with a filling and covered by a sauce in Italian cuisine."
+            var t4 = "Lasagne are wide, flat-shaped pasta, and possibly one of the oldest types of pasta."
+            var t5 = "Macaroni and cheese—also called mac and cheese in American is a dish of English origin, consisting of cooked macaroni pasta and a cheese sauce, most commonly cheddar."
+            var t6 = "Arrabbiata sauce, or sugo all'arrabbiata in Italian, is a spicy sauce for pasta made from garlic, tomatoes, and red chili peppers cooked in olive oil. Arrabbiata literally means \"angry\" in Italian; the name of the sauce refers to the spiciness of the chili peppers."
+
         	$("#butt2").css({
         	"background-color" : "rgba(0, 255, 0, 0.2)"
         	});
         	$("#firstimg").attr("src","images/Pasta/pic02.jpg");
         	$("#firstimgpop").attr("src","images/Pasta/pic02.jpg");
+
+		
+
         	$("#secondimg").attr("src","images/Pasta/pic01.jpg");
         	$("#secondimgpop").attr("src","images/Pasta/pic01.jpg");
+
         	$("#thirdimg").attr("src","images/Pasta/pic04.jpg");
         	$("#thirdimgpop").attr("src","images/Pasta/pic04.jpg");
         	$("#fourthimg").attr("src","images/Pasta/pic03.jpg");
@@ -71,11 +113,32 @@ function changes(){
         	$("#sixthimg").attr("src","images/Pasta/pic05.jpg");
         	$("#sixthimgpop").attr("src","images/Pasta/pic05.jpg");
 
-        	replace_text("WOOORKS","sbcjhbsdjhbcshdbcjhsdbchjbsdhcjhsdbcjh",$("#firstbox"), $("#openModal1"));
-
+        	replace_text(h1,t1,$("#firstbox"), $("#openModal1"));	
+		replace_text(h2,t2,$("#secondbox"), $("#openModal2"));
+		replace_text(h3,t3,$("#thirdbox"), $("#openModal3"));
+		replace_text(h4,t4,$("#fourthbox"), $("#openModal4"));
+		replace_text(h5,t5,$("#fifthbox"), $("#openModal5"));
+		replace_text(h6,t5,$("#sixthbox"), $("#openModal6"));
+		
 			break;
 
 		case 2:
+
+		var h1 = "Salmon"
+		var h2 = "Herring"
+		var h3 = "Vegetables Bath"
+		var h4 = "Ravioli"
+		var h5 = "Aspargus Wonder"
+		var h6 = "Chef's Creation"
+		
+		var t1 = "Delicious salmon with limes and green pesto. Perfect to treat your taste buds !"
+		var t2 = "Figs and herring. This is meant to treat you because you only deserve the best."
+		var t3 = "Surrounded by vegetables this is our offer for the health concious customer. "
+		var t4 = "Our home made ravioli with some freshly catched salmon is are sure to amaze you."
+		var t5 = "The celebrate the spring we have seasonal offer of aspargus which you can enjoy for a limited time only." 
+		var t6 = "The chefs personal creation. Salmon with zucchini and potatoes is our restaurants speciality. Be sure to try it."
+
+
 			$("#butt3").css({
         	"background-color" : "rgba(0, 255, 0, 0.2)"
         	});
@@ -92,9 +155,32 @@ function changes(){
         	$("#sixthimg").attr("src","images/Meat_Fish/pic05.jpg");
         	$("#sixthimgpop").attr("src","images/Meat_Fish/pic05.jpg");
 
+		replace_text(h1,t1,$("#firstbox"), $("#openModal1"));	
+		replace_text(h2,t2,$("#secondbox"), $("#openModal2"));
+		replace_text(h3,t3,$("#thirdbox"), $("#openModal3"));
+		replace_text(h4,t4,$("#fourthbox"), $("#openModal4"));
+		replace_text(h5,t5,$("#fifthbox"), $("#openModal5"));
+		replace_text(h6,t5,$("#sixthbox"), $("#openModal6"));
+
 			break;
 
 		case 3:
+
+		var h1 = ""
+		var h2 = ""
+		var h3 = ""
+		var h4 = ""
+		var h5 = ""
+		var h6 = ""
+		
+		var t1 = ""
+		var t2 = ""
+		var t3 = ""
+		var t4 = ""
+		var t5 = "" 
+		var t6 = ""
+
+		
 			$("#butt4").css({
         	"background-color" : "rgba(0, 255, 0, 0.2)"
         	});
@@ -110,6 +196,13 @@ function changes(){
         	$("#fifthimgpop").attr("src","images/Dessert/pic06.jpg");
         	$("#sixthimg").attr("src","images/Dessert/pic05.jpg");
         	$("#sixthimgpop").attr("src","images/Dessert/pic05.jpg");
+
+		replace_text(h1,t1,$("#firstbox"), $("#openModal1"));	
+		replace_text(h2,t2,$("#secondbox"), $("#openModal2"));
+		replace_text(h3,t3,$("#thirdbox"), $("#openModal3"));
+		replace_text(h4,t4,$("#fourthbox"), $("#openModal4"));
+		replace_text(h5,t5,$("#fifthbox"), $("#openModal5"));
+		replace_text(h6,t5,$("#sixthbox"), $("#openModal6"));
 
 			break;
 
